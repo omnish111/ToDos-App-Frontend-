af1fca0fecd3424a0b5e9b9c3354e4a8d1cb6ea8
omnish111 <omnishkasundra@gmail.com>
2026-04-14 17:39:26 +0530

    second commit

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { getStoredToken } from "../utils/storage";
 
-const configuredBaseUrl = import.meta.env.VITE_API_BASE_URL;
-const fallbackBaseUrl = "http://localhost:5000/api";
+const configuredBaseUrl = (import.meta.env.VITE_API_BASE_URL || "").replace(
+  /\/$/,
+  "",
+);
+const fallbackBaseUrl = import.meta.env.DEV ? "http://localhost:5001/api" : "/api";
 
 const apiClient = axios.create({
-  baseURL: (configuredBaseUrl || fallbackBaseUrl).replace(/\/$/, ""),
+  baseURL: configuredBaseUrl || fallbackBaseUrl,
 });
 
 apiClient.interceptors.request.use((config) => {
